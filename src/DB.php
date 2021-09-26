d05f27da6a2d5460e8fac64feb60f52ddbfa733e
Nanok Charles <nanok@192.168.1.20>
2021-09-26 09:34:03 +0000

    Base classes added

---
+++ src/DB.php
@@ -0,0 +1,89 @@
+<?php
+
+    namespace Goa\Middleware\Database;
+
+    class DB
+    {
+        const SGBD_SQLITE = "sqlite";
+        const SGBD_MYSQL = "sqlite";
+
+        /**
+         * @param array $config Database configs. Must contains at least 'sgbd' field
+         */
+        public function __construct(Array $config)
+        {
+            try {
+                switch ($config['sgbd']) {
+                    case DB::SGBD_SQLITE:
+                        $this->db = new \PDO($config['sgbd'] . ":" . dirname(__FILE__) . "/" . $config['file']);
+                        break;
+
+                    case DB::SGBD_MYSQL:
+                        $this->db = new \PDO($config['sgbd'] . ":host=" . $config['host'] . ";dbname=" . $config['dbname'], $config['username'], $config['password']);
+                        break;
+                    
+                    default:
+                        throw new \Exception("SGBD specified in config is not supported", 1);
+                        break;
+                }
+                $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            } catch (\Exception $th) {
+                throw $th;
+            }
+        }
+
+        /**
+         * Function to make a select request
+         * @param String $query SQL query
+         * @param Array $data Array which contains the data
+         */
+        public function select(String $query, Array $data = [])
+        {
+            try {
+                $statement = $this->db->prepare($query);
+
+                foreach ($data as $key => $value) {
+                    $statement->bindParam(":" . $key, $data[$key]);
+                }
+
+                $statement->execute();
+
+                return $statement->fetchAll();
+
+            } catch (\Exception $th) {
+                throw $th;
+            }
+        }
+
+        /**
+         * Function to make a insert, update or delete request
+         * @param String $query SQL query
+         * @param Array $data Array which contains the data
+         */
+        public function set(String $query, Array $data)
+        {
+            try {
+                $statement = $this->db->prepare($query);
+
+                foreach ($data as $key => $value) {
+                    $statement->bindParam(":" . $key, $data[$key]);
+                }
+
+                $statement->execute();
+
+                return true;
+
+            } catch (\Exception $th) {
+                throw $th;
+            }
+            
+            return false;
+        }
+
+        public function getLastId()
+        {
+            return $this->db->lastInsertId();
+        }
+    }
+
+?>
---
+++ src/Model.php
@@ -0,0 +1,237 @@
+<?php
+
+    namespace Goa\Middleware\Database;
+
+    abstract class Model implements ModelFunction
+    {
+
+        protected $db;
+        protected $tablename;
+
+        /**
+         * A global variable $config which contains database configs must be defined
+         * @param string $tablename Name of the table linked by the model
+         */
+        public function __construct(String $tablename)
+        {
+            global $config;
+            
+            $this->db = new DB($config);
+            $this->tablename = $tablename;
+        }
+
+        /**
+         * Function which insert a model in a table of database
+         */
+        protected function insertModel()
+        {
+            try {
+                $data = (Array) $this;
+                unset($data[array_key_last($data)]);
+
+                $query = "INSERT INTO " . $this->tablename . "(";
+                $values = "(";
+
+                foreach ($data as $key => $value) {
+                    if (!empty($value) && !str_contains($key, "*") && (empty($value) || $value != -1))
+                    {
+                        $query = $query . $key . ",";
+                        $values = $values . ":" . $key . ",";
+                        continue;
+                    }
+                    
+                    unset($data[$key]);
+                }
+
+                $query = substr($query, 0, -1);
+                $values = substr($values, 0, -1);
+
+                $query = $query . ") VALUES" . $values . ")";
+
+                error_log($query);
+                error_log(json_encode($data));
+
+                return $this->db->set($query, $data);
+            } catch (\Throwable $th) {
+                throw $th;
+            }
+        }
+
+        /**
+         * Function which update a model in a table of database
+         * @param mixed $id_name Table primary key name
+         * @param $as_many_id Is true if the table as many field as primary key else false
+         */
+        protected function updateModel($id_name, $as_many_id = false)
+        {
+            try {
+                $data = (Array) $this;
+                unset($data[array_key_last($data)]);
+
+                $query = "UPDATE " . $this->tablename . " SET ";
+
+                foreach ($data as $key => $value) {
+                    if (!str_contains($key, "*"))
+                    {
+                        if ($key != $id_name)
+                            $query = $query . $key . "=:" . $key . ",";
+
+                        continue;
+                    }
+                    unset($data[$key]);
+                }
+
+                $query = substr($query, 0, -1);
+                $query = $query . " WHERE ";
+
+                if ($as_many_id)
+                {
+                    foreach ($id_name as $key => $value) {
+                        $query = $query . $id_name . "=:" . $id_name;
+                        $query = $query . " AND ";
+                    }
+
+                    $query = substr($query, 0, -5);
+                }
+                else
+                {
+                    $query = $query . $id_name . "=:" . $id_name;
+                }
+
+                return $this->db->set($query, $data);
+            } catch (\Throwable $th) {
+                throw $th;
+            }
+        }
+
+        /**
+         * Function which delete a model in a table of database
+         * @param mixed $id_name Table primary key name
+         * @param $as_many_id Is true if the table as many field as primary key else false
+         */
+        protected function deleteModel($id_name, $as_many_id = false)
+        {
+            try {
+                $data = (Array) $this;
+                $query = "DELETE FROM " . $this->tablename . " WHERE ";
+
+                if ($as_many_id)
+                {
+                    foreach ($id_name as $key => $value) {
+                        $query = $query . $id_name . "=:" . $id_name;
+                        $query = $query . " AND ";
+                    }
+
+                    $query = substr($query, 0, -5);
+                }
+                else
+                {
+                    $query = $query . $id_name . "=:" . $id_name;
+                }
+                
+                $data = [
+                    $id_name => $data[$id_name],
+                ];
+
+                return $this->db->set($query, $data);
+            } catch (\Throwable $th) {
+                throw $th;
+            }
+        }
+
+        /**
+         * Function which get all line in a table of database
+         * @param $class Model class
+         */
+        public function getAll($class)
+        {
+            try {
+                $result = $this->db->select("SELECT * FROM " . $this->tablename);
+
+                $output = [];
+                foreach ($result as $key => $value) {
+                    $output[] = new $class($value);
+                }
+
+                return $output;
+            } catch (\Throwable $th) {
+                throw $th;
+            }
+        }
+
+        /**
+         * Function which get a line in a table of database from his id
+         * @param $class Model class
+         */        
+        public function getById($class, Array $id)
+        {
+            try {
+                $query = "SELECT * FROM " . $this->tablename ." WHERE ";
+
+                foreach ($id as $key => $value) {
+                    $query = $query . $key . "=:" . $key . " AND ";
+                }
+                $query = substr($query, 0, -5);
+
+                $result = $this->db->select($query, $id);
+
+                $output = [];
+                foreach ($result as $key => $value) {
+                    $output[] = new $class($value);
+                }
+
+                if (count($output) == 1)
+                {
+                    $output = $output[0];
+                }
+                return $output;
+            } catch (\Throwable $th) {
+                throw $th;
+            }
+        }
+
+        public function getLastId()
+        {
+            return $this->db->getLastId();
+        }
+
+    }
+
+
+
+    namespace Goa\Middleware\Database;
+
+    interface ModelFunction {
+
+        /**
+         * Function which save a model in database
+         */
+        public function save();
+
+        /**
+         * Function which update a model data in database
+         */
+        public function update();
+
+        /**
+         * Function which delete a model data in database
+         */
+        public function delete();
+
+    }
+
+
+    namespace Goa\Middleware\Database;
+
+    interface ModelInterface
+    {
+        public function all();
+        public function findById(Array $id);
+        public function save();
+        public function update();
+        public function delete();
+    }
+
+    
+
+?>
